088868b1202f2404c140e7163b08789b43849ce0
yumikokh <ymymyummy@gmail.com>
2014-11-18 07:44:39 +0900

    カテゴリ閉じるボタン追加、gulp整えた

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,39 @@
 var gulp = require('gulp'),
 sass = require('gulp-ruby-sass'),
 connect = require('gulp-connect'),
-// spritesmith = require('gulp.spritesmith'),
 autoprefixer = require('gulp-autoprefixer'),
 plumber = require('gulp-plumber'),
-notify = require('gulp-notify');
-// compass = require('gulp-compass'),
-// minifyCSS = require('gulp-minify-css');
+notify = require('gulp-notify'),
+csslint = require('gulp-csslint'),
+cssmin  = require('gulp-cssmin'),
+header  = require('gulp-header'),
+minifyCSS = require('gulp-minify-css');
 
 
 
-// gulp.task('minify-css', function() {
-//   gulp.src('./css/*.css')
-//   .pipe(minifyCSS({keepBreaks:true}))
-//   .pipe(gulp.dest('./dist/'))
-// });
+gulp.task('minify-css', function() {
+  gulp.src('./css/*.css')
+  .pipe(minifyCSS({keepBreaks:true}))
+  .pipe(gulp.dest('./dist/'));
+});
 
-//
+
 gulp.task('styles',function(){
-  gulp.src('./sass/*.scss','./sass/fontawesome/*.scss')
+  gulp.src(['./sass/*.scss','./sass/fontawesome/*.scss'])
   .pipe(sass({
     // style: 'compressed',
     style: 'compact',
     sourcemap: true
   }))
+  .pipe(csslint())
+  // .pipe(csslint.reporter())
   .pipe(autoprefixer('last 2 version', 'ie 9', 'ie 8', 'Firefox ESR'))
+  .pipe(header('@charset "utf-8";\n\n'))
   .pipe(gulp.dest('./css'))
   .pipe(connect.reload())
   .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}));
 });
 
-//スプライド画像生成
-// gulp.task('sprite', function () {
-//   var spriteData = gulp.src('./img/sprite/*.png') //スプライトにする愉快な画像達
-//   .pipe(spritesmith({
-//     imgName: 'sprite.png', //スプライトの画像
-//     cssName: '_sprite.scss', //生成されるscss
-//     imgPath: '../img/sprite.png', //生成されるscssに記載されるパス
-//     cssFormat: 'scss', //フォーマット
-//     cssVarMap: function (sprite) {
-//       // sprite.name = 'sprite-' + sprite.name; //VarMap(生成されるScssにいろいろな変数の一覧を生成)
-//     }
-//   }));
-//   spriteData.img.pipe(gulp.dest('./img/')); //imgNameで指定したスプライト画像の保存先
-//   spriteData.css.pipe(gulp.dest('./sass/')); //cssNameで指定したcssの保存先
-// });
 
 //ローカルサーバー
 gulp.task('connect',function(){
